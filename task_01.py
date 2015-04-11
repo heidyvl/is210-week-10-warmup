104ed009806f1a0d0030d3f8535412bd8443b13d
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-11 17:39:46 +0000

    task_01

---
+++ task_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Nested Dict Definition"""
+
+
+GRADE_DATA = {
+    "Luke Skywalker": {
+        "math": "B",
+        "etiquette": "B+",
+        "grammar": "B",
+        "gym": "A"
+        },
+    "Han Solo": {
+        "math": "A-",
+        "etiquette": "C-",
+        "grammar": "B",
+        "gym": "B"
+        },
+    "C-3PO": {
+        "math": "C",
+        "etiquette": "A+",
+        "grammar": "A",
+        "gym": "F"
+        }
+}
